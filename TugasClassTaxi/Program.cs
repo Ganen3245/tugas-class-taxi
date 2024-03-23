d3dab6f5b6980e7bf995a92f28809c47d1d72279
ganendra <ganendra12@students.amikom.ac.id>
2024-03-23 15:09:17 +0700

    first commit

---
+++ TugasClassTaxi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassTaxi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.driverName = "Jono";
+            taxi.onDuty = true;
+            taxi.numPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TugasClassTaxi/Taxi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassTaxi
+{
+    internal class Taxi
+    {
+        public string driverName { get; set; }
+        public bool onDuty { get; set; }
+        public int numPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", driverName);
+            Console.WriteLine("On Duty: {0}", onDuty ? "Yes" : "No");
+            Console.WriteLine("Number of Passenger: {0}", numPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", driverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai menjemput penumpang", driverName);
+        }
+    }
+
+}
